cf47bb90488e0adc97eaaacd4403569459d4b3af
husban ahmad <130596470+husban088@users.noreply.github.com>
2023-12-10 08:16:35 +0500

    todo list

--- main.js
+++ main.js
@@ -3,37 +3,42 @@ window.addEventListener('load', () => {
 	const input = document.querySelector("#new-task-input");
 	const list_el = document.querySelector("#tasks");
 
-	form.addEventListener('submit', function(e) {
+	form.addEventListener('submit', function (e) {
 		e.preventDefault();
 
 		const task = input.value;
 
-		const task_el = document.createElement('div');
-		task_el.classList.add('task');
+		if (!task) {
+			alert("Please fill the task!");
+			return;
+		}
 
-		const task_content_el = document.createElement('div');
-		task_content_el.classList.add('content');
+		const task_el = document.createElement("div");
+		task_el.classList.add("task");
+
+		const task_content_el = document.createElement("div");
+		task_content_el.classList.add("content");
 
 		task_el.appendChild(task_content_el);
 
-		const task_input_el = document.createElement('input');
-		task_input_el.classList.add('text');
-		task_input_el.type = 'text';
+		const task_input_el = document.createElement("input");
+		task_input_el.classList.add("text");
+		task_input_el.type = "text";
 		task_input_el.value = task;
-		task_input_el.setAttribute('readonly', 'readonly');
+		task_input_el.setAttribute("readonly", "readonly");
 
 		task_content_el.appendChild(task_input_el);
 
-		const task_actions_el = document.createElement('div');
-		task_actions_el.classList.add('actions');
-		
-		const task_edit_el = document.createElement('button');
-		task_edit_el.classList.add('edit');
-		task_edit_el.innerText = 'Edit';
+		const task_actions_el = document.createElement("div");
+		task_actions_el.classList.add("actions");
 
-		const task_delete_el = document.createElement('button');
-		task_delete_el.classList.add('delete');
-		task_delete_el.innerText = 'Delete';
+		const task_edit_el = document.createElement('button');
+		task_edit_el.classList.add("edit");
+		task_edit_el.innerText = "Edit";
+
+		const task_delete_el = document.createElement("button");
+		task_delete_el.classList.add("delete");
+		task_delete_el.innerText = "Delete";
 
 		task_actions_el.appendChild(task_edit_el);
 		task_actions_el.appendChild(task_delete_el);
@@ -44,7 +49,7 @@ window.addEventListener('load', () => {
 
 		input.value = '';
 
-		task_edit_el.addEventListener('click', function(e) {
+		task_edit_el.addEventListener('click', function (e) {
 			if (task_edit_el.innerText.toLowerCase() == "edit") {
 				task_edit_el.innerText = "Save";
 				task_input_el.removeAttribute("readonly");
@@ -55,7 +60,7 @@ window.addEventListener('load', () => {
 			}
 		});
 
-		task_delete_el.addEventListener('click', function(e) {
+		task_delete_el.addEventListener('click', function (e) {
 			list_el.removeChild(task_el);
 		});
 	});
